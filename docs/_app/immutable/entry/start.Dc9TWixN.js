82b55eae8311e0d2077eb0986465fd167e40e43f
juanfuc <juanfuc@gmail.com>
2025-10-21 13:59:38 -0500

    update preload hero and body cover

--- docs/_app/immutable/entry/start.Dc9TWixN.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/C_xarXya.js";export{o as load_css,r as start};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,25 @@
 	<link rel="stylesheet" href="/assets/site.css" />
 	<title>Comisión Corográfica Digital</title>
 	<meta name="description" content="Sitio general de la CCD: ontología, recursos y exposición." />
+	<link
+	rel="preload"
+	as="image"
+	href="/assets/imgs/0002-800.webp"
+	imagesrcset="/assets/imgs/0002-800.webp 800w, /assets/imgs/0002-1600.webp 1600w"
+	imagesizes="100vw"
+	type="image/webp"
+	fetchpriority="high"
+	/>
+
+	<link
+	rel="preload"
+	as="image"
+	href="/assets/imgs/0001-800.webp"
+	imagesrcset="/assets/imgs/0001-800.webp 800w, /assets/imgs/0001-1600.webp 1600w"
+	imagesizes="100vw"
+	type="image/webp"
+	/>
+
 </svelte:head>
 
 
